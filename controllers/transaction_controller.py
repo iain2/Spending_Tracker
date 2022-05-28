7ccfe2e40ed7be89424c31dc6e6ab35fe4fb1b49
Iain Hay <iainhay@Iains-MacBook-Air.local>
2022-05-28 10:53:47 +0100

    transaction page styling

--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -1,3 +1,4 @@
+from gettext import translation
 from flask import Flask, redirect, render_template, Blueprint
 import repositories.merchant_repository as merchant_repository
 import repositories.tag_repository as tag_repository
@@ -10,3 +11,14 @@ transaction_blueprint = Blueprint("Transactions", __name__)
 def books():
     transactions = transaction_repository.select_all()
     return render_template("transactions/index.html", transactions=transactions)
+
+
+# 'POST' form for add new transactions
+# drop down list for tags and merchants
+
+# form for new tags
+
+# form for new merchants
+
+# total spent
+# select_all loop through add translation.amount to total return total
