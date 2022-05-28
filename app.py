51822654bab757557c32e25fdf205002db2c035f
Iain Hay <iainhay@Iains-MacBook-Air.local>
2022-05-28 15:00:34 +0100

    merchant page, add merchant

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from controllers.transaction_controller import transaction_blueprint
+from controllers.merchant_controller import merchant_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(transaction_blueprint)
+app.register_blueprint(merchant_blueprint)
 
 
 @app.route("/")
---
+++ controllers/merchant_controller.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, render_template, Blueprint, request
+from models.merchant import Merchant
+from models.transaction import *
+import repositories.merchant_repository as merchant_repository
+import repositories.tag_repository as tag_repository
+import repositories.transaction_repository as transaction_repository
+
+merchant_blueprint = Blueprint("Merchants", __name__)
+
+
+@merchant_blueprint.route("/merchants")
+def transaction():
+
+    merchants = merchant_repository.select_all()
+
+    return render_template(
+        "merchants/index.html",
+        merchants=merchants,
+    )
+
+
+@merchant_blueprint.route("/merchants", methods=["POST"])
+def create_transaction():
+    merchant_name = request.form["merchant"]
+    merchant = Merchant(merchant_name)
+    merchant_repository.save(merchant)
+    return redirect("/merchants")
